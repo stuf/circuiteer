d8e6314197cfdd98ba9b0ceeb44c64155b12b749
Stefan Rimaila <stefan@rimaila.fi>
2021-05-04 00:30:00 +0300

    stuff

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fill: theme => ({
+        none: 'none',
+        white: theme('colors.white'),
+        red: theme('colors.red.500'),
+        black: theme('colors.black'),
+      }),
+      stroke: theme => ({
+        none: 'none',
+        white: theme('colors.white'),
+        black: theme('colors.black'),
+        red: theme('colors.red.500'),
+        green: theme('colors.green.500'),
+      }),
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
