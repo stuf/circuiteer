0b396b4b4e4d8d04ab8cb52983479ebc46d6f3bc
Stefan Rimaila <stefan@rimaila.fi>
2021-05-05 22:36:49 +0300

    add new components

---
+++ src/_/Dropdown.js
@@ -0,0 +1,95 @@
+import cx from 'classnames';
+import * as P from 'prop-types';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+
+import css from './Dropdown.module.css';
+
+function Dropdown(props) {
+  const { label, value, choices, onChange } = props;
+
+  return (
+    <Listbox {...{ value, onChange }} as="div" className={cx(css.root)}>
+      {({ open }) => (
+        <>
+          <div className="relative">
+            <Listbox.Label>{label}</Listbox.Label>
+
+            <Listbox.Button className={css.button}>
+              <span className={css.buttonText}>{value.label}</span>
+              <span className={css.buttonIconWrap}>
+                <SelectorIcon className={css.buttonIcon} aria-hidden="true" />
+              </span>
+            </Listbox.Button>
+
+            <Transition
+              show={open}
+              enter={css.enter}
+              enterFrom={css.enterFrom}
+              enterTo={css.enterTo}
+              leave={css.leave}
+              leaveFrom={css.leaveFrom}
+              leaveTo={css.leaveTo}
+            >
+              <Listbox.Options static className={css.choices}>
+                {choices.map((choice, ix) => (
+                  <Listbox.Option
+                    key={`choice-${ix}`}
+                    value={choice}
+                    disabled={choice.unavailable}
+                  >
+                    {({ active, selected }) => (
+                      <>
+                        <span
+                          className={cx(css.choice, {
+                            [css.active]: active,
+                            [css.selected]: selected,
+                            [css.unavailable]: choice.unavailable,
+                          })}
+                        >
+                          {choice.label}
+                        </span>
+                        {selected ? (
+                          <span className={css.selectedIconWrap}>
+                            <CheckIcon
+                              aria-hidden="true"
+                              className={css.selectedIcon}
+                            />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
+  );
+}
+
+Dropdown.propTypes = {
+  value: P.shape({
+    id: P.string,
+    label: P.string,
+  }),
+  choices: P.arrayOf(
+    P.shape({
+      id: P.string,
+      label: P.string,
+    }),
+  ),
+};
+
+Dropdown.defaultProps = {
+  value: {},
+  choices: [
+    { id: 'one', label: 'One' },
+    { id: 'two', label: 'Two', unavailable: true },
+    { id: 'three', label: 'Three' },
+  ],
+};
+
+export default Dropdown;
--- src/_/Sidebar.js
+++ src/_/Sidebar.js
@@ -1,72 +1,131 @@
 import * as L from 'partial.lenses';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 
+import Dropdown from '_/Dropdown';
 import Input from '../components/form/Input';
-import { setDragSize, setDragging, setDrag } from '../state/drag';
+import { setDrag } from '../state/drag';
+import { setGridSize } from 'state/grid';
+
+import css from './Sidebar.module.css';
 
 function Sidebar(props) {
   const update = useDispatch();
-  const [gw, gh] = useSelector(L.get(['grid', 'size']), shallowEqual);
+  const gxy = useSelector(L.get(['grid', 'size']), shallowEqual);
+  const { currentLocation, locations } = useSelector(
+    L.get([
+      'location',
+      L.valueOr({}),
+      L.pick({
+        currentLocation: 'current',
+        locations: [
+          'locations',
+          L.array(
+            L.pick({
+              value: 'id',
+              label: 'displayName',
+            }),
+          ),
+        ],
+      }),
+    ]),
+    shallowEqual,
+  );
   const modules = useSelector(
     L.get(['module', 'tier', L.valueOr({})]),
     shallowEqual,
   );
 
+  const current = locations.find(x => x.value === currentLocation);
+
+  const [gw, gh] = gxy;
+
   return (
-    <div>
-      <section>
-        <header>Modules</header>
+    <div className={css.root}>
+      <section className={css.group}>
+        <header className={css.groupHead}>Location</header>
 
-        {Object.entries(modules).map(([t, xs]) => (
-          <details key={`tier-${t}`} className="px-2 py-1" open>
-            <summary>Tier {t}</summary>
-
-            <ul className="grid grid-cols-2 gap-2 text-xs">
-              {xs.map((x, i) => (
-                <li
-                  key={`tier-${t}-${i}`}
-                  draggable
-                  onDragStart={e => {
-                    const data_ = { module: x };
-                    const data = JSON.stringify(data_);
-                    e.dataTransfer.dropEffect = 'copy';
-                    e.dataTransfer.setData('application/json', data);
-
-                    console.log('data', data);
-
-                    update(
-                      setDrag({
-                        dragging: true,
-                        size: data_.module.size,
-                      }),
-                    );
-                  }}
-                  className="border border-pink-500 px-2 py-1 font-mono"
-                >
-                  <div className="flex justify-between">
-                    <span>{x.shortId}</span>
-                    <span
-                      className={`${
-                        x.power < 0 ? 'text-red-500' : 'text-green-500'
-                      }`}
-                    >
-                      {x.power}
-                    </span>
-                  </div>
-                  <div>{x.size.join(' × ')}</div>
-                </li>
-              ))}
-            </ul>
-          </details>
-        ))}
+        <div className={css.groupBody}>
+          <Dropdown
+            value={current}
+            choices={locations}
+            onChange={e => {
+              console.log('change', e);
+            }}
+          />
+        </div>
       </section>
 
-      <section>
-        <header>Grid</header>
+      <section className={css.group}>
+        <header className={css.groupHead}>Modules</header>
 
-        <div className="grid grid-cols-2 gap-4">
-          <Input label="Width" type="number" value={gw} />
-          <Input label="Height" type="number" value={gh} />
+        <div className={css.groupBody}>
+          {Object.entries(modules).map(([t, xs]) => (
+            <details key={`tier-${t}`} open>
+              <summary>Tier {t}</summary>
+
+              <ul className="grid grid-cols-2 gap-2 text-xs">
+                {xs.map((x, i) => (
+                  <li
+                    key={`tier-${t}-${i}`}
+                    draggable
+                    onDragStart={e => {
+                      const data_ = { module: x };
+                      const data = JSON.stringify(data_);
+                      e.dataTransfer.dropEffect = 'copy';
+                      e.dataTransfer.setData('application/json', data);
+
+                      console.log('data', data);
+
+                      update(
+                        setDrag({
+                          dragging: true,
+                          size: data_.module.size,
+                        }),
+                      );
+                    }}
+                    className="border border-gray-200 shadow hover:shadow-md px-2 py-1 font-mono"
+                  >
+                    <div className="flex justify-between">
+                      <span>{x.shortId}</span>
+                      <span
+                        className={`${
+                          x.power < 0 ? 'text-red-500' : 'text-green-500'
+                        }`}
+                      >
+                        {x.power}
+                      </span>
+                    </div>
+                    <div>{x.size.join(' × ')}</div>
+                  </li>
+                ))}
+              </ul>
+            </details>
+          ))}
+        </div>
+      </section>
+
+      <section className={css.group}>
+        <header className={css.groupHead}>Grid</header>
+
+        <div className={css.groupBody}>
+          <div className="grid grid-cols-2 gap-4">
+            <Input
+              label="Width"
+              type="number"
+              value={gw}
+              onChange={e =>
+                update(setGridSize(L.set(0, +e.target.value, gxy)))
+              }
+            />
+            <Input
+              label="Height"
+              type="number"
+              value={gh}
+              onChange={e =>
+                update(setGridSize(L.set(1, +e.target.value, gxy)))
+              }
+            />
+          </div>
         </div>
       </section>
     </div>
