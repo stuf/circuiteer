7eb17fd03c8e18fe5c27b20a9bba5cec14e39262
Stefan Rimaila <stefan@rimaila.fi>
2021-05-07 23:58:35 +0300

    chore: improve visuals for sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { LightningBoltIcon } from '@heroicons/react/solid';
 import cx from 'classnames';
 
 import css from './App.module.css';
@@ -21,16 +22,26 @@ function App() {
       </header>
 
       <div className="flex h-full relative">
-        <div className="flex-1 relative h-full">
-          {showEditor && flags.showEditor && (
-            <div className="absolute top-2 right-2 bg-white rounded-md border-2 shadow-lg w-56">
-              <EntityEditor />
-            </div>
-          )}
+        <div className="flex-1 flex flex-col">
+          <div className="border-b-2 px-4 py-1">
+            <button className="px-4 py-2 bg-black text-white rounded-md relative">
+              <span className="pl-6">Power</span>
+              <span className="absolute inset-y-0 left-2 inline-flex items-center">
+                <LightningBoltIcon className="w-5 h-5" />
+              </span>
+            </button>
+          </div>
+          <div className="flex-1 relative">
+            {showEditor && flags.showEditor && (
+              <div className="absolute top-2 right-2 bg-white rounded-md border-2 shadow-lg w-56">
+                <EntityEditor />
+              </div>
+            )}
 
-          {flags.showPowerStatus && <PowerMeter />}
+            {flags.showPowerStatus && <PowerMeter />}
 
-          <Canvas />
+            <Canvas />
+          </div>
         </div>
         <div className="border-l-2 w-72 divide-y-2 h-full overflow-auto">
           <Sidebar />
--- src/_/Sidebar/Location.js
+++ src/_/Sidebar/Location.js
@@ -1,9 +1,10 @@
-import * as L from 'partial.lenses';
+import { useMemo } from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import { SelectorIcon } from '@heroicons/react/solid';
+import * as L from 'partial.lenses';
 
 import Group from '_/Group';
 import { changeLocation } from 'state/location';
-import { useMemo } from 'react';
 
 export default function Location() {
   const update = useDispatch();
@@ -34,22 +35,24 @@ export default function Location() {
 
   return (
     <Group title="Location">
-      <select
-        className="px-4 py-2 appearance-none block w-full rounded-md bg-white border-2"
-        value={current.value}
-        onChange={e => {
-          console.log(e.target.value);
-          update(changeLocation(+e.target.value));
-        }}
-      >
-        {locations.map((loc, i) => {
-          return (
-            <option key={i} className="px-4 py-2" value={loc.value}>
-              {loc.label}
-            </option>
-          );
-        })}
-      </select>
+      <div className="relative shadow-md rounded-md border-2 hover:shadow-lg bg-white">
+        <span className="absolute inset-y-0 right-2 inline-flex items-center">
+          <SelectorIcon className="w-5 h-5" />
+        </span>
+        <select
+          className="px-4 py-2 appearance-none block w-full bg-transparent"
+          value={current.value}
+          onChange={e => update(changeLocation(+e.target.value))}
+        >
+          {locations.map((loc, i) => {
+            return (
+              <option key={i} className="px-4 py-2" value={loc.value}>
+                {loc.label}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     </Group>
   );
 }
