982f445be88be62a35e3f7ab82192157b842d051
Stefan Rimaila <stefan@rimaila.fi>
2021-05-08 00:48:43 +0300

    chore: improve main UI

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import PowerMeter from 'canvas/_/PowerMeter';
 import Options from './_/Options';
 import Sidebar from './_/Sidebar';
 import EntityEditor from './_/EntityEditor';
+import Button from '_/Button';
 import { showSelectedEntityIn, optionFlagsIn } from 'common/selectors';
 
 function App() {
@@ -17,20 +18,23 @@ function App() {
   return (
     <main className="h-full flex flex-col overflow-hidden">
       <header className={cx(css.mainHeader, 'border-b-2')}>
+        <header className="font-black pl-4 flex">
+          <div>Circuiteer</div>
+          <div className="text-xxs font-normal pl-2">Experimental</div>
+        </header>
+
         <div className="flex-1" />
+
         <Options />
       </header>
 
       <div className="flex h-full relative">
         <div className="flex-1 flex flex-col">
-          <div className="border-b-2 px-4 py-1">
-            <button className="px-4 py-2 bg-black text-white rounded-md relative">
-              <span className="pl-6">Power</span>
-              <span className="absolute inset-y-0 left-2 inline-flex items-center">
-                <LightningBoltIcon className="w-5 h-5" />
-              </span>
-            </button>
+          <div className="border-b-2 px-4 py-1 space-x-1">
+            <Button icon={LightningBoltIcon}>Power</Button>
+            <Button>Poop</Button>
           </div>
+
           <div className="flex-1 relative">
             {showEditor && flags.showEditor && (
               <div className="absolute top-2 right-2 bg-white rounded-md border-2 shadow-lg w-56">
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontSize: {
+        xxs: ['0.6rem', { lineHeight: '1rem' }],
+      },
       fontFamily: theme => ({
         sans: ['Inter'],
       }),
