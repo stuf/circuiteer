633f1bcc34d03d182fbab2b1c9a37dbb62dec5bb
Stefan Rimaila <stefan@rimaila.fi>
2021-05-08 01:04:43 +0300

    feat: add routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Splash from './Splash';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 
 function Root() {
   return (
     <Provider {...{ store }}>
-      <App />
+      <Router>
+        <Switch>
+          <Route path="/editor">
+            <App />
+          </Route>
+          <Route>
+            <Splash />
+          </Route>
+        </Switch>
+      </Router>
     </Provider>
   );
 }
