f17b53f830241f809896d51d8f232d1c702fdc11
Stefan Rimaila <stefan@rimaila.fi>
2021-05-08 21:19:19 +0300

    chore: style

--- src/store.js
+++ src/store.js
@@ -7,7 +7,7 @@ const preloadedState = {
     dragging: false,
     entities: [
       {
-        pos: [16, 8],
+        pos: [2, 2],
         module: {
           id: 'largeDummyGenerator',
           shortId: 'ldg',
@@ -20,7 +20,7 @@ const preloadedState = {
         enabled: false,
       },
       {
-        pos: [21, 8],
+        pos: [7, 2],
         module: {
           id: 'largeDummyConsumer',
           shortId: 'ldc',
@@ -33,7 +33,7 @@ const preloadedState = {
         enabled: true,
       },
       {
-        pos: [16, 13],
+        pos: [2, 7],
         module: {
           id: 'largeDummyConsumer',
           shortId: 'ldc',
@@ -45,13 +45,7 @@ const preloadedState = {
         id: '2fb98a1d-0738-4def-bf52-f0c91a0e1e45',
         enabled: false,
       },
-      {
-        pos: [21, 13],
-        id: '378699e0-3242-42cc-916f-b7ee5cfe4ced',
-        enabled: true,
-      },
     ],
-    current: '378699e0-3242-42cc-916f-b7ee5cfe4ced',
   },
   drag: {
     dragging: false,
@@ -61,76 +55,6 @@ const preloadedState = {
   grid: {
     size: [32, 32],
   },
-  // module: {
-  //   tier: {
-  //     1: [
-  //       {
-  //         id: 'dummyGenerator',
-  //         shortId: 'dg',
-  //         name: 'Generator (Dummy)',
-  //         power: 5,
-  //         tier: 1,
-  //         size: [2, 2],
-  //       },
-  //       {
-  //         id: 'dummyConsumer',
-  //         shortId: 'dc',
-  //         name: 'Consumer (Dummy)',
-  //         power: -2.5,
-  //         tier: 1,
-  //         size: [2, 2],
-  //       },
-  //     ],
-  //     2: [
-  //       {
-  //         id: 'mediumDummy',
-  //         shortId: 'md',
-  //         name: 'Medium (Dummy)',
-  //         power: 0,
-  //         tier: 2,
-  //         size: [4, 4],
-  //       },
-  //     ],
-  //     3: [
-  //       {
-  //         id: 'largeDummyConsumer',
-  //         shortId: 'ldc',
-  //         name: 'Large Consumer (Dummy)',
-  //         power: -12,
-  //         tier: 3,
-  //         size: [4, 4],
-  //       },
-  //       {
-  //         id: 'largeDummyGenerator',
-  //         shortId: 'ldg',
-  //         name: 'Large Generator (Dummy)',
-  //         power: 11,
-  //         tier: 3,
-  //         size: [4, 4],
-  //       },
-  //     ],
-  //     4: [
-  //       {
-  //         id: 'xlargeDummy',
-  //         shortId: 'xld',
-  //         name: 'X-Large (Dummy)',
-  //         power: 0,
-  //         tier: 4,
-  //         size: [8, 8],
-  //       },
-  //     ],
-  //   },
-  // },
-  // location: {
-  //   locations: [
-  //     {
-  //       id: 1,
-  //       name: 'firstPlanet',
-  //       displayName: 'First Planet',
-  //     },
-  //   ],
-  //   current: 1,
-  // },
 };
 
 const store = configureStore({ preloadedState, reducer: reducers });
