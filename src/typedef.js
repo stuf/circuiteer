3e37b621bfd779dafddb088ed1be4dcdf565faa9
Stefan Rimaila <stefan@rimaila.fi>
2021-05-08 21:07:55 +0300

    feat: add typedefs

---
+++ src/typedef.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {object} ModuleObject
+ * @prop {string} id
+ * @prop {string} shortId
+ * @prop {string} name
+ * @prop {number} power
+ * @prop {tier} number
+ * @prop {number[]} size
+ */
