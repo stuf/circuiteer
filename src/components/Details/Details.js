612c0536efd3cc85c7bbcddd8660cd444a2d171c
Stefan Rimaila <stefan@rimaila.fi>
2021-05-12 19:33:16 +0300

    chore: spring cleanup

---
+++ src/components/Details/Details.js
@@ -0,0 +1,57 @@
+import { Disclosure, Transition } from '@headlessui/react';
+import { ChevronRightIcon } from '@heroicons/react/solid';
+import cx from 'classnames';
+
+import './Details.css';
+
+export function Details(props) {
+  const { open, head, children, className } = props;
+
+  return (
+    <div className="w-full max-w-md mx-auto bg-white overflow-hidden rounded-lg text-sm">
+      <Disclosure
+        className={cx('details details--root', className)}
+        defaultOpen={open}
+      >
+        {({ open }) => (
+          <>
+            <Disclosure.Button
+              className={cx(
+                'flex justify-between w-full px-4 py-2 rounded-md',
+                'bg-purple-500',
+                'text-white',
+                'focus:outline-none',
+                'focus-visible:ring',
+                'focus-visible:ring-purple-500',
+                'focus-visible:ring-opacity-75',
+              )}
+            >
+              <span>{head}</span>
+
+              <ChevronRightIcon
+                className={cx(
+                  open && 'transform rotate-90',
+                  'w-5 h-5 text-white',
+                  'transition-transform duration-200 ease-out',
+                )}
+              />
+            </Disclosure.Button>
+
+            <Transition
+              show={open}
+              className="mt-2"
+              enter="details--enter"
+              enterFrom="details--enter-from"
+              enterTo="details--enter-to"
+              leave="details--leave"
+              leaveFrom="details--leave-from"
+              leaveTo="details--leave-to"
+            >
+              <Disclosure.Panel static>{children}</Disclosure.Panel>
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+    </div>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,4 @@
-// export { default as Canvas } from './Canvas';
-// export { default as Crosshair } from './Crosshair';
-// export { default as EntityObject } from './EntityObject';
-// export { default as Sidebar } from './Sidebar';
-// export { default as PowerMeter } from './PowerMeter';
-
 export { Button } from './Button';
 export { Toggle } from './Toggle';
+export { Details } from './Details';
+export { Input } from './Input';
