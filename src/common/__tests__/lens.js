19234bee68ef7ee381d95a54ec74c45f3e9e0f4c
Stefan Rimaila <stefan@rimaila.fi>
2021-05-14 19:33:22 +0300

    feat(common): add lens isos

--- src/common/__tests__/lens.js
+++ src/common/__tests__/lens.js
@@ -1,15 +1,35 @@
 import * as L from 'partial.lenses';
-import { gridI } from '../lens';
+import { Matrix } from '../linear';
+import { asGridPointI, gridI, matrixI } from '../lens';
 
 describe('common/lens', () => {
-  test('gridI', () => {
-    const m = [4, 4];
-    const iso = gridI(m);
+  test('matrixI', () => {
+    const p1 = [1, 2];
 
-    const p1 = [16, 16];
-    const p2 = [32, 32];
+    // Get
+    const r1 = L.get(matrixI, p1);
+    expect(r1).toBeInstanceOf(Matrix);
+    expect(r1.data).toEqual([[1, 2]]);
 
-    expect(L.get(iso, p1)).toEqual([4, 4]);
-    expect(L.get(iso, p2)).toEqual([8, 8]);
+    // Set
+    const s1 = L.set(matrixI, r1.mulEach(5), r1);
+    expect(s1).not.toBeInstanceOf(Matrix);
+    expect(s1).toEqual([5, 10]);
+  });
+
+  test('asGridPointI', () => {
+    const p1 = [96, 192];
+    const gxy = new Matrix([24, 24]);
+    const [_px, _py] = p1;
+    const [_gx, _gy] = gxy.data.flat();
+
+    const pointI = asGridPointI(gxy);
+
+    const r1 = L.get(pointI, p1);
+    expect(r1.data).toEqual([[_px / _gx, _py / _gy]]);
+
+    const a2 = new Matrix([4, 8]);
+    const r2 = L.set(pointI, a2, p1);
+    expect(r2).toEqual([(_px / _gx) * _gx, (_py / _gy) * _gy]);
   });
 });
--- src/common/lens.js
+++ src/common/lens.js
@@ -1,9 +1,35 @@
+import * as R from 'ramda';
 import * as L from 'partial.lenses';
+import { Matrix } from 'common/linear';
 
 const { round } = Math;
+
+const construct1 = R.constructN(1);
 
 export const gridI = ([gw, gh]) =>
   L.iso(
     ([x, y]) => [round(x / gw), round(y / gh)],
     ([x, y]) => [x * gw, y * gh],
   );
+
+export const matrixI = L.iso(
+  construct1(Matrix),
+  R.pipe(R.prop('data'), R.invoker(0, 'flat')),
+);
+
+/**
+ * Handle a screen-space point as a grid-space point.
+ * Performs rounding, so loss of precision will happen
+ * when coordinates are between [0..gridX] and [0..gridY].
+ *
+ * @param {Point} gxy
+ * @param {Function} roundingFn
+ * @returns
+ */
+export const asGridPointI = (gxy, roundingFn = Math.round) => [
+  matrixI,
+  L.iso(
+    R.pipe(R.invoker(1, 'div')(gxy), R.map(roundingFn)),
+    R.invoker(1, 'mul')(gxy),
+  ),
+];
