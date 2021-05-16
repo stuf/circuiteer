9c0cf9925b3af1a13f913dde70ad937e24cb21bf
Stefan Rimaila <stefan@rimaila.fi>
2021-05-16 21:49:13 +0300

    build: add storybook config folder

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.story.jsx?',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+};
