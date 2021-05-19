f584a6ce67d1c163384e6cb3a7573ac29d995f1c
Stefan Rimaila <stefan@rimaila.fi>
2021-05-19 23:30:28 +0300

    refactor(ui): relocate Icon-related styling to its component

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -3,11 +3,19 @@ import * as P from 'prop-types';
 import './Icon.css';
 
 export function Icon(props) {
-  const { name } = props;
+  const { name, size = 24 } = props;
 
   return (
-    <span className="icon icon--ui">
-      <span className="material-icons material-icons-outlined">{name}</span>
+    <span
+      className="icon icon--uiflex items-center justify-center"
+      style={{ width: size, height: size }}
+    >
+      <span
+        className="material-icons material-icons-outlined"
+        style={{ fontSize: size }}
+      >
+        {name}
+      </span>
     </span>
   );
 }
