e19a91fd13593ffae290eb39b7dc78eeb8723cf0
Stefan Rimaila <stefan@rimaila.fi>
2021-05-19 19:42:48 +0300

    feat: implement patterns

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import * as Views from './views';
+import { DefineSvgAssets } from './core/svg';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/sink" component={Views.Sink} />
-        <Route path="/editor" component={Views.Editor} />
-        <Route path="/" component={Views.Splash} />
-      </Switch>
-    </Router>
+    <>
+      <DefineSvgAssets />
+      <Router>
+        <Switch>
+          <Route path="/sink" component={Views.Sink} />
+          <Route path="/editor" component={Views.Editor} />
+          <Route path="/" component={Views.Splash} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
