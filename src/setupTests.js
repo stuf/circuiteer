2159002500d907a0df627f845355c455dd947415
Stefan Rimaila <stefan@rimaila.fi>
2021-05-30 14:57:15 +0300

    test: automock i18next

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,15 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import 'jest-extended';
+
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: str => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
