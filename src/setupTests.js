b206ea83489f520ffe78dba40c76c699ead69e57
Stefan Rimaila <stefan@rimaila.fi>
2021-05-31 00:14:35 +0300

    test: add ResizeObserver polyfill

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,9 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import 'jest-extended';
+import ResizeObserver from 'resize-observer-polyfill';
+
+window.ResizeObserver = ResizeObserver;
 
 jest.mock('react-i18next', () => ({
   // this mock makes sure any components using the translate hook can use it without a warning being shown
