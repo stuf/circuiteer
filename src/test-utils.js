c0ab9277bf2ddb1ec38673a8de34348457c8d288
Stefan Rimaila <stefan@rimaila.fi>
2021-05-30 14:57:02 +0300

    test: add test utils for redux bindings

---
+++ src/test-utils.js
@@ -0,0 +1,42 @@
+import { render as renderRtl } from '@testing-library/react';
+import { format } from 'util';
+import { Provider } from 'react-redux';
+import { preloadedState } from './core/preloaded';
+import * as reducers from './state';
+import { configureStore } from '@reduxjs/toolkit';
+
+//
+
+export function render(vdom, { initialState, store, ...renderOptions } = {}) {
+  const rootStore =
+    store ||
+    configureStore({
+      preloadedState: initialState || preloadedState,
+      reducer: reducers,
+    });
+
+  function Wrapper({ children }) {
+    return <Provider store={rootStore}>{children}</Provider>;
+  }
+
+  return renderRtl(vdom, { wrapper: Wrapper, ...renderOptions });
+}
+
+//
+
+export function suppressPropTypeWarnings() {
+  const err = console.error.bind(console.error);
+
+  beforeAll(() => {
+    console.error = (msg, ...rest) => {
+      const errorMsg = format(msg, ...rest);
+      if (!errorMsg.toString().includes('Warning: Failed prop type')) {
+        err(errorMsg);
+      }
+    };
+  });
+
+  afterAll(() => {
+    console.error = err;
+  });
+}
