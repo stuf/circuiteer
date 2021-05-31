67201c4cc6c2ddf4ed4dac0c9a66f45bca86bc30
Stefan Rimaila <stefan@rimaila.fi>
2021-05-31 10:06:02 +0300

    test: add Infobar tests

--- src/containers/Infobar/__tests__/Infobar.js
+++ src/containers/Infobar/__tests__/Infobar.js
@@ -1,8 +1,34 @@
+import * as L from 'partial.lenses';
+import { fireEvent, logRoles } from '@testing-library/react';
+
 import { render } from 'test-utils';
+import { preloadedState } from 'core/preloaded';
 import { Infobar } from '../Infobar';
 
 describe('Infobar', () => {
-  test('works', () => {
-    const { container } = render(<Infobar />);
+  it('works by default', async () => {
+    const { container, findByRole } = render(<Infobar />);
+
+    // logRoles(container);
+
+    expect(container).toMatchSnapshot();
+
+    const buttons = [
+      await findByRole('button', { name: /power/i }),
+      await findByRole('button', { name: /import/i }),
+      await findByRole('button', { name: /export/i }),
+      await findByRole('button', { name: /copy/i }),
+    ];
+
+    buttons.forEach(button => fireEvent.click(button));
+  });
+
+  it('allows to show efficiency as word instead of raw value', () => {
+    const initialState = L.set(
+      ['options', 'flags', 'showEfficiencyAsMultiplier'],
+      false,
+      preloadedState,
+    );
+    const { container } = render(<Infobar />, { initialState });
   });
 });
