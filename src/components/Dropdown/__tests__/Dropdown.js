802e0448c28979791a5a96ac4e2c998f1f0724e7
Stefan Rimaila <stefan@rimaila.fi>
2021-06-01 11:03:46 +0300

    test(ui): add missing coverage test for Dropdown

--- src/components/Dropdown/__tests__/Dropdown.js
+++ src/components/Dropdown/__tests__/Dropdown.js
@@ -1,24 +1,38 @@
 import { render, fireEvent, logRoles, logDOM } from '@testing-library/react';
 import { Dropdown } from '../Dropdown';
 
-test('Dropdown', async () => {
-  const onChange = jest.fn();
-  const props = {
-    choices: [
-      { label: 'One', value: 'one' },
-      { label: 'Two', value: 'two' },
-    ],
-    onChange,
-  };
+describe('Dropdown', () => {
+  it('works as default', async () => {
+    const onChange = jest.fn();
+    const props = {
+      choices: [
+        { label: 'One', value: 'one' },
+        { label: 'Two', value: 'two' },
+      ],
+      onChange,
+    };
 
-  const { container, findByRole } = render(<Dropdown {...props} />);
+    const { container, findByRole } = render(<Dropdown {...props} />);
 
-  const input = await findByRole('combobox');
+    const input = await findByRole('combobox');
 
-  fireEvent.change(input, { target: { value: 'two' } });
+    fireEvent.change(input, { target: { value: 'two' } });
 
-  expect(onChange).toHaveBeenCalled();
-  expect(input.value).toBe('two');
+    expect(onChange).toHaveBeenCalled();
+    expect(input.value).toBe('two');
 
-  expect(container).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('can be disabled', async () => {
+    const props = {
+      choices: [],
+      disabled: true,
+      onChange: () => {},
+    };
+
+    const { container, findByRole } = render(<Dropdown {...props} />);
+
+    expect(await findByRole('combobox')).toBeDisabled();
+  });
 });
