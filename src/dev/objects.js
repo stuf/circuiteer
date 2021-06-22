b24f8fa26e0b10c213dc70c8e9b16a3a9be8122d
Stefan Rimaila <stefan@rimaila.fi>
2021-06-22 19:52:17 +0300

    chore: add dev assets

---
+++ src/dev/objects.js
@@ -0,0 +1,24 @@
+const objects = [
+  {
+    id: '8kxEna2bJCrNEqrsOXJ7T',
+    pos: { x: 100, y: 100 },
+    size: { width: 100, height: 100 },
+  },
+  {
+    id: 'fQqPP39PZ7GwWD3uEvWht',
+    pos: { x: 150, y: 150 },
+    size: { width: 200, height: 200 },
+  },
+  {
+    id: 'YM-fecGxY2T4JG6zGj4HB',
+    pos: { x: 200, y: 200 },
+    size: { width: 200, height: 200 },
+  },
+  {
+    id: '9a76TVdtJ2ypFxQ5rsiam',
+    pos: { x: 250, y: 250 },
+    size: { width: 100, height: 100 },
+  },
+];
+
+export default objects;
