7c02b64b1f51ef34af895bf9469b1a08aca6c7da
Stefan Rimaila <stefan@rimaila.fi>
2021-06-22 19:51:56 +0300

    feat(views): add selectors to mainview for game objects

--- src/views/Main/MainView.js
+++ src/views/Main/MainView.js
@@ -1,20 +1,37 @@
-import { Canvas } from 'components/Canvas';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { useCanvasObjects } from 'common/hooks/objects';
+import { AutosizeCanvas } from 'components/Canvas';
+import { ModulePalette } from 'components/ModulePalette';
+import { updateObject } from 'state/objects';
 
 export function MainView(props) {
+  const { objects, ids, entities } = useCanvasObjects();
+  const update = useDispatch();
+
+  console.log({ objects, ids, entities });
+
+  const onDragStop = useCallback(
+    (e, o) => {
+      console.log('onDragStop: update object', o);
+      update(updateObject(o));
+    },
+    [update],
+  );
+
+  const onDragStart = useCallback((e, o) => {
+    console.log('onDragStart in MainView', { e, o });
+    console.log('onDragStart');
+  }, []);
+
+  if (!objects?.length) {
+    return <div>nothing</div>;
+  }
+
   return (
     <main className="view">
-      <Canvas />
-
-      <div className="toolbar absolute bottom-right">
-        <header>Modules</header>
-
-        <div>
-          <ul className="toolbar__items">
-            <li>one</li>
-            <li>two</li>
-          </ul>
-        </div>
-      </div>
+      <AutosizeCanvas objects={objects} options={{ onDragStop, onDragStart }} />
+      <ModulePalette className="bottom-right absolute" />
     </main>
   );
 }
