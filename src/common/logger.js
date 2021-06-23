533c21abfab60dbc0b73352970d1e4a65239d276
Stefan Rimaila <stefan@rimaila.fi>
2021-06-23 13:21:37 +0300

    feat(common): add logger

---
+++ src/common/logger.js
@@ -0,0 +1,30 @@
+import winston, { format, transports } from 'winston';
+
+const { combine, timestamp, label, splat, printf } = format;
+
+const logFormat = printf(({ level, message, label, timestamp }) => {
+  const linePrefix = [timestamp, `[${label}]`, level];
+
+  const line = [linePrefix.join(' '), message];
+
+  const out = line.join(': ');
+
+  return out;
+});
+
+const loggerOptions = labelName => ({
+  format: combine(label({ label: labelName }), splat(), timestamp(), logFormat),
+  transports: [new transports.Console()],
+});
+
+const createLogger = name => {
+  winston.loggers.add(name, loggerOptions(name));
+
+  const logger = winston.loggers.get(name);
+
+  return logger;
+};
+
+export const appLog = createLogger('app');
+export const canvasLog = createLogger('canvas');
+export const storeLog = createLogger('store');
