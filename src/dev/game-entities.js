2d31ab218ab99cad9a876dcc099c09fd7e3e7ce2
Stefan Rimaila <stefan@rimaila.fi>
2021-06-24 22:59:59 +0300

    chore(dev): add placeholder data

---
+++ src/dev/game-entities.js
@@ -0,0 +1,7 @@
+export const objects = [
+  { id: 'qtRtg', tier: 1, power: 1, powerType: 'constant' },
+  { id: 'rtg', tier: 2, power: 4, powerType: 'constant' },
+  { id: 'mediumPrinter', tier: 2, power: -2, powerType: 'onDemand' },
+  { id: 'largePrinter', tier: 3, power: -5, powerType: 'onDemand' },
+  { id: 'soilCentrifuge', tier: 3, power: -6, powerType: 'onDemand' },
+];
--- src/dev/objects.js
+++ src/dev/objects.js
@@ -1,23 +1,42 @@
+/**
+ * @type {Data.CanvasObject[]}
+ */
 const objects = [
   {
     id: '8kxEna2bJCrNEqrsOXJ7T',
     pos: { x: 100, y: 100 },
     size: { width: 100, height: 100 },
+    entity: 'qtRtg',
+    disabled: true,
   },
   {
     id: 'fQqPP39PZ7GwWD3uEvWht',
-    pos: { x: 150, y: 150 },
+    pos: { x: 220, y: 100 },
     size: { width: 200, height: 200 },
+    entity: 'qtRtg',
   },
   {
     id: 'YM-fecGxY2T4JG6zGj4HB',
-    pos: { x: 200, y: 200 },
+    pos: { x: 100, y: 220 },
     size: { width: 200, height: 200 },
+    entity: 'rtg',
+    locked: true,
   },
   {
     id: '9a76TVdtJ2ypFxQ5rsiam',
-    pos: { x: 250, y: 250 },
+    pos: { x: 100, y: 400 },
     size: { width: 100, height: 100 },
+    entity: 'rtg',
+  },
+  {
+    id: 'E9964Wxvwy8yQ_AHNfyQt',
+    pos: { x: 500, y: 100 },
+    entity: 'soilCentrifuge',
+  },
+  {
+    id: '-6eosX3-UmbNlmrzq5gMk',
+    pos: { x: 840, y: 100 },
+    entity: 'soilCentrifuge',
   },
 ];
 
