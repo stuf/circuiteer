b612e5fc446a998446f3f6b970d2f00722dbd31b
Stefan Rimaila <stefan@rimaila.fi>
2021-06-25 00:55:04 +0300

    chore: add default size to dev data

--- src/dev/game-entities.js
+++ src/dev/game-entities.js
@@ -1,7 +1,33 @@
+import { DefaultSize } from 'common/defaults';
+
 export const objects = [
-  { id: 'qtRtg', tier: 1, power: 1, powerType: 'constant' },
-  { id: 'rtg', tier: 2, power: 4, powerType: 'constant' },
-  { id: 'mediumPrinter', tier: 2, power: -2, powerType: 'onDemand' },
-  { id: 'largePrinter', tier: 3, power: -5, powerType: 'onDemand' },
-  { id: 'soilCentrifuge', tier: 3, power: -6, powerType: 'onDemand' },
+  {
+    id: 'qtRtg',
+    tier: 1,
+    power: 1,
+    powerType: 'constant',
+    size: DefaultSize[1],
+  },
+  { id: 'rtg', tier: 2, power: 4, powerType: 'constant', size: DefaultSize[2] },
+  {
+    id: 'mediumPrinter',
+    tier: 2,
+    power: -2,
+    powerType: 'onDemand',
+    size: DefaultSize[2],
+  },
+  {
+    id: 'largePrinter',
+    tier: 3,
+    power: -5,
+    powerType: 'onDemand',
+    size: DefaultSize[3],
+  },
+  {
+    id: 'soilCentrifuge',
+    tier: 3,
+    power: -6,
+    powerType: 'onDemand',
+    size: DefaultSize[4],
+  },
 ];
