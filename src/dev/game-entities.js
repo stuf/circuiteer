3b6a6bd8290721870e10786143804218fc9e4896
Stefan Rimaila <stefan@rimaila.fi>
2021-06-29 22:52:24 +0300

    build: update initial data

--- src/dev/game-entities.js
+++ src/dev/game-entities.js
@@ -1,35 +1,50 @@
 import { DefaultSize } from 'common/defaults';
 
+const small = { tier: 1, size: DefaultSize[1] };
+const medium = { tier: 2, size: DefaultSize[2] };
+const large = { tier: 3, size: DefaultSize[3] };
+const xlarge = { tier: 4, size: DefaultSize[4] };
+
 export const objects = [
   {
     id: 'qtRtg',
-    tier: 1,
     power: 1,
     powerType: 'constant',
-    size: DefaultSize[1],
-  },
-  { id: 'rtg', tier: 2, power: 4, powerType: 'constant', size: DefaultSize[2] },
-  {
-    id: 'mediumPrinter',
-    tier: 2,
-    power: -2,
-    powerType: 'onDemand',
-    size: DefaultSize[2],
+    ...small,
   },
   {
-    id: 'largePrinter',
-    tier: 3,
-    power: -5,
-    powerType: 'onDemand',
-    size: DefaultSize[3],
+    id: 'smallGenerator',
+    power: 2,
+    powerType: 'powered',
+    ...small,
   },
   {
-    id: 'soilCentrifuge',
-    tier: 3,
-    power: -6,
-    powerType: 'onDemand',
-    size: DefaultSize[3],
+    id: 'smallSolarPanel',
+    power: 1,
+    powerType: 'solar',
+    ...small,
   },
+  {
+    id: 'smallWindTurbine',
+    power: 1.5,
+    powerType: 'wind',
+    ...small,
+  },
+  {
+    id: 'powerCells',
+    power: 1,
+    powerType: 'powered',
+    ...small,
+  },
+  {
+    id: 'smallBattery',
+    power: 1,
+    powerType: 'constant',
+    ...small,
+  },
+  { id: 'rtg', power: 4, powerType: 'constant', ...medium },
+  { id: 'fieldShelter', power: 1, powerType: 'constant', ...medium },
+  { id: 'mediumGenerator', power: 9, powerType: 'powered', ...medium },
   {
     id: 'mediumWindTurbine',
     tier: 2,
@@ -43,5 +58,49 @@ export const objects = [
     power: 4,
     powerType: 'solar',
     size: DefaultSize[2],
+  },
+  {
+    id: 'mediumPrinter',
+    power: -2,
+    powerType: 'onDemand',
+    ...medium,
+  },
+  { id: 'largeSolarPanel', power: 8, powerType: 'solar', ...large },
+  { id: 'largeWindTurbine', power: 10, powerType: 'wind', ...large },
+  {
+    id: 'largePrinter',
+    power: -5,
+    powerType: 'onDemand',
+    ...large,
+  },
+  {
+    id: 'smeltingFurnace',
+    power: -5,
+    powerType: 'onDemand',
+    ...large,
+  },
+  {
+    id: 'chemistryLab',
+    power: -10,
+    powerType: 'onDemand',
+    ...large,
+  },
+  {
+    id: 'soilCentrifuge',
+    power: -6,
+    powerType: 'onDemand',
+    ...large,
+  },
+  {
+    id: 'atmosphericCondenser',
+    power: -20,
+    powerType: 'onDemand',
+    ...large,
+  },
+  {
+    id: 'autoExtractor',
+    power: -8,
+    powerType: 'onDemand',
+    ...xlarge,
   },
 ];
---
+++ src/dev/tier.js
@@ -0,0 +1,9 @@
+const defaults = [
+  { id: 1, name: 'small' },
+  { id: 2, name: 'medium' },
+  { id: 3, name: 'large' },
+  { id: 4, name: 'extraLarge' },
+  { id: -1, name: 'other' },
+];
+
+export default defaults;
