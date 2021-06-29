71eb49c91bcf0b3c40de83036b6610cb1aab0395
Stefan Rimaila <stefan@rimaila.fi>
2021-06-29 13:30:35 +0300

    feat(components): implement common svg patterns

--- src/components/svg/Patterns.js
+++ src/components/svg/Patterns.js
@@ -1,11 +1,12 @@
-import { PatternLines } from '@visx/pattern';
+import { PatternLines, PatternCircles } from '@visx/pattern';
 import { Pattern as P } from 'common/constants';
 import { Pattern } from 'common/settings';
 
 export function Patterns(props) {
   return (
-    <svg>
+    <svg className="off-canvas">
       <PatternLines id={P.DIAGONAL} {...Pattern[P.DIAGONAL]} />
+      <PatternCircles id={P.DOTTED_GRID} {...Pattern[P.DOTTED_GRID]} />
     </svg>
   );
 }
