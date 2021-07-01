1341de3d34cf226a7ec922995abee13ec0696c24
Stefan Rimaila <stefan@rimaila.fi>
2021-07-01 18:56:31 +0300

    feat(common): add fallback default size for misc

--- src/common/defaults.js
+++ src/common/defaults.js
@@ -10,6 +10,7 @@ export const SizeScale = {
   2: 2,
   3: 4,
   4: 8,
+  '-1': 1,
 };
 
 export const DefaultSize = {
@@ -17,4 +18,5 @@ export const DefaultSize = {
   2: S.mul.concat(BaseSize, S.mul.of(SizeScale[2])),
   3: S.mul.concat(BaseSize, S.mul.of(SizeScale[3])),
   4: S.mul.concat(BaseSize, S.mul.of(SizeScale[4])),
+  '-1': S.mul.concat(BaseSize, S.mul.of(SizeScale[-1])),
 };
